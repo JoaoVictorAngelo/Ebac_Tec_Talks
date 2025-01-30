bbdac11fc372f0a75984d51c321d2ca81cc8fe20
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2025-01-30 07:55:39 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('ola mundo')
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStempDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStempAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStempDoEvento - timeStempAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+    const segundosEmMs = 1000
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / segundosEmMs);
+    
+    console.log(diasAteOEvento)
+    console.log(horasAteOEvento)
+    console.log(minutosAteOEvento)
+    console.log(segundosAteOEvento)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if(distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `<br>O Evento já acabou <br> &#128546; &#128557; &#128546; `
+    }
+}, 1000)
